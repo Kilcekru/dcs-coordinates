6257c0ac77ea6a857931d94fb8cb58977448495f
Andreas Lackinger <andreas.lackinger@unkreativ.at>
2023-07-30 16:13:31 +0200

    feat: added sinai

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,13 +2,15 @@ import { Grid, MapName } from "../types";
 import caucasus from "./grid-caucasus.json";
 import normandy from "./grid-normandy.json";
 import persianGulf from "./grid-persianGulf.json";
+import sinai from "./grid-sinai.json";
 import southAtlantic from "./grid-southAtlantic.json";
 import syria from "./grid-syria.json";
 
 export const data: Record<MapName, Grid> = {
 	caucasus: caucasus as Grid,
 	normandy: normandy as Grid,
-	syria: syria as Grid,
 	persianGulf: persianGulf as Grid,
+	sinai: sinai as Grid,
 	southAtlantic: southAtlantic as Grid,
+	syria: syria as Grid,
 };
---
+++ test/sinai/LOtoLL.test.ts
@@ -0,0 +1,73 @@
+import { describe, expect, test } from "@jest/globals";
+
+import { LOtoLL } from "../../src";
+
+describe("sinai", () => {
+	describe("LOtoLL", () => {
+		test("x too small", () => {
+			expect(() => LOtoLL({ map: "sinai", x: -250_001, z: 0 })).toThrow(
+				new Error("Coordinates outside of acceptable area for sinai")
+			);
+		});
+
+		test("x too big", () => {
+			expect(() => LOtoLL({ map: "sinai", x: 330_000, z: 0 })).toThrow(
+				new Error("Coordinates outside of acceptable area for sinai")
+			);
+		});
+
+		test("z too small", () => {
+			expect(() => LOtoLL({ map: "sinai", x: 0, z: -150_001 })).toThrow(
+				new Error("Coordinates outside of acceptable area for sinai")
+			);
+		});
+
+		test("z too big", () => {
+			expect(() => LOtoLL({ map: "sinai", x: 0, z: 470_000 })).toThrow(
+				new Error("Coordinates outside of acceptable area for sinai")
+			);
+		});
+
+		test("corner NE", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: 329_999, z: 469_999 });
+			expect(lat).toBeCloseTo(32.99478, 5);
+			expect(lng).toBeCloseTo(36.21888, 5);
+		});
+
+		test("corner SE", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: -250_000, z: 469_999 });
+			expect(lat).toBeCloseTo(27.76868, 5);
+			expect(lng).toBeCloseTo(36.05177, 5);
+		});
+
+		test("corner SW", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: -250_000, z: -150_000 });
+			expect(lat).toBeCloseTo(27.76443, 5);
+			expect(lng).toBeCloseTo(29.76132, 5);
+		});
+
+		test("corner NW", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: 329_999, z: -150_000 });
+			expect(lat).toBeCloseTo(32.98954, 5);
+			expect(lng).toBeCloseTo(29.58402, 5);
+		});
+
+		test("Cairo-West", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: 7_372, z: -33_544 });
+			expect(lat).toBeCloseTo(30.10857, 5);
+			expect(lng).toBeCloseTo(30.8956, 5);
+		});
+
+		test("Ramon", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: 80_508, z: 328_814 });
+			expect(lat).toBeCloseTo(30.77469, 5);
+			expect(lng).toBeCloseTo(34.66769, 5);
+		});
+
+		test("Palmahim", () => {
+			const { lat, lng } = LOtoLL({ map: "sinai", x: 205_484, z: 329_304 });
+			expect(lat).toBeCloseTo(31.90176, 5);
+			expect(lng).toBeCloseTo(34.69288, 5);
+		});
+	});
+});
